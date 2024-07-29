144d9ab91aff4ce862833b732eaea447399861eb
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-07-29 21:13:49 +0300

    Fixed jest config.

--- jest.config.ts
+++ jest.config.ts
@@ -2,8 +2,8 @@ import type { Config } from 'jest';
 
 const config: Config = {
   testEnvironment: 'node',
-  modulePathIgnorePatterns: ['<rootDir>/src/', '<rootDir>/(dist-e2e|e2e)/_util.js'],
-  testMatch: ['<rootDir>/e2e/**/*.js', '<rootDir>/dist-e2e/**/*.spec.js'],
+  modulePathIgnorePatterns: ['<rootDir>/src/', '<rootDir>/e2e/'],
+  testMatch: ['<rootDir>/dist-e2e/**/*.spec.js'],
 };
 
 export default config;
